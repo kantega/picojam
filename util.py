495815debc2e06eda89ceec825dd06307f31d5ed
Edvard Neset Karlsen <edvard.karlsen@kantega.no>
2023-04-14 17:23:27 +0200

    add util.py

---
+++ util.py
@@ -0,0 +1,182 @@
+import framebuf
+import utime
+from machine import Pin, PWM, SPI
+
+
+BL = 13  
+CS = 9
+DC = 8
+MOSI = 11
+RST = 12
+SCK = 10
+
+
+class LCD(framebuf.FrameBuffer):
+    def __init__(self):
+        self.width = 240
+        self.height = 240
+        
+        self.cs = Pin(CS, Pin.OUT)
+        self.rst = Pin(RST, Pin.OUT)
+        
+        self.cs(1)
+        self.spi = SPI(1)
+        self.spi = SPI(1, 1000_000)
+        self.spi = SPI(1, 100000_000, polarity=0, phase=0, sck=Pin(SCK), mosi=Pin(MOSI), miso=None)
+        self.dc = Pin(DC,Pin.OUT)
+        self.dc(1)
+        self.buffer = bytearray(self.height * self.width * 2)
+        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        self.init_display()
+        
+        self.red   = 0x07E0
+        self.green = 0x001f
+        self.blue  = 0xf800
+        self.white = 0xffff
+
+        # set screen brightness
+        pwm = PWM(Pin(BL)) 
+        pwm.freq(1000)
+        pwm.duty_u16(32768) # max 65535 - mid value
+        
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)  
+
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
+
+    def init_display(self):
+        self.rst(1)
+        self.rst(0)
+        self.rst(1)
+        
+        self.write_cmd(0x36)
+        self.write_data(0x70)
+
+        self.write_cmd(0x3A) 
+        self.write_data(0x05)
+
+        self.write_cmd(0xB2)
+        self.write_data(0x0C)
+        self.write_data(0x0C)
+        self.write_data(0x00)
+        self.write_data(0x33)
+        self.write_data(0x33)
+
+        self.write_cmd(0xB7)
+        self.write_data(0x35) 
+
+        self.write_cmd(0xBB)
+        self.write_data(0x19)
+
+        self.write_cmd(0xC0)
+        self.write_data(0x2C)
+
+        self.write_cmd(0xC2)
+        self.write_data(0x01)
+
+        self.write_cmd(0xC3)
+        self.write_data(0x12)   
+
+        self.write_cmd(0xC4)
+        self.write_data(0x20)
+
+        self.write_cmd(0xC6)
+        self.write_data(0x0F) 
+
+        self.write_cmd(0xD0)
+        self.write_data(0xA4)
+        self.write_data(0xA1)
+
+        self.write_cmd(0xE0)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0D)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2B)
+        self.write_data(0x3F)
+        self.write_data(0x54)
+        self.write_data(0x4C)
+        self.write_data(0x18)
+        self.write_data(0x0D)
+        self.write_data(0x0B)
+        self.write_data(0x1F)
+        self.write_data(0x23)
+
+        self.write_cmd(0xE1)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0C)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2C)
+        self.write_data(0x3F)
+        self.write_data(0x44)
+        self.write_data(0x51)
+        self.write_data(0x2F)
+        self.write_data(0x1F)
+        self.write_data(0x1F)
+        self.write_data(0x20)
+        self.write_data(0x23)
+        
+        self.write_cmd(0x21)
+
+        self.write_cmd(0x11)
+
+        self.write_cmd(0x29)
+
+    def show(self):
+        self.write_cmd(0x2A)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xef)
+        
+        self.write_cmd(0x2B)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xEF)
+        
+        self.write_cmd(0x2C)
+        
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(self.buffer)
+        self.cs(1)
+
+
+def color_rgb(red: uint, green: uint, blue: uint) -> uint: 
+    """Convert RGB888 to RGB565."""
+    return (
+        ((green & 0b00011100) << 3) + 
+        (((blue & 0b11111000) >> 3) << 8) +
+        (red & 0b11111000) + 
+        ((green & 0b11100000) >> 5)
+    )
+
+
+class BuggyFPSEstimator:
+    def __init__(self):
+        self._sample_start_time = utime.time_ns()
+        self._num_frames_since_reset = 0
+
+    def update_and_get(self):
+        self._num_frames_since_reset += 1
+        time_now = utime.time_ns()
+        sample_duration = max(0.01, (time_now - self._sample_start_time)/1E9)
+        estimate = int(round(self._num_frames_since_reset / sample_duration))
+        if sample_duration > 10:
+            self._sample_start_time = time_now
+            self._num_frames_since_reset = 0
+        return estimate
